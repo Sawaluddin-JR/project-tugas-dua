7481c8a177518840a6f017f282140658f1bd694e
Sawaluddin <sawaluddinsrg472@gmail.com>
2023-05-29 21:21:17 +0700

    commit pertama

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+//import Tombol from "./Tombol";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+const nama = 'Sawaluddin';
+
+function Header(){
+    return (
+        <header>{nama}</header>
+    );
+}
+
+export default Header;
